6cee3dd202aa815546dd3e0469efc7791dea9511
ErnestoXG <ggoku02@gmail.com>
2021-12-27 00:08:03 +0000

    Exercises Up To 60

--- exercises/053-joinArrays/app.js
+++ exercises/053-joinArrays/app.js
@@ -1,3 +1,8 @@
 function joinArrays(arr1, arr2) {
   // your code here
+  arr2.forEach(e => {
+    arr1.push(e)
+  })
+
+  return arr1
 }
--- exercises/055-getElementsUpTo/app.js
+++ exercises/055-getElementsUpTo/app.js
@@ -1,3 +1,8 @@
 function getElementsUpTo(array, n) {
   // your code here
+   array.splice(n)
+   return array
 }
+
+var output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
+console.log(output); // --> ['a', 'b', 'c']
