5e58d6d48b92a4d81fd5ff46e6661cff9e5c545e
ErnestoXG <ggoku02@gmail.com>
2021-12-27 01:08:04 +0000

    Up to 62

--- exercises/061-countCharacter/app.js
+++ exercises/061-countCharacter/app.js
@@ -1,3 +1,9 @@
 function countCharacter(str, char) {
     // your code here
+    var re = new RegExp(char, 'g');
+    foundCharArr = str.match(re)
+    return foundCharArr.length
 }
+
+var output = countCharacter('I am a hacker', 'a');
+console.log(output); // --> 3
--- exercises/062-getAllLetters/app.js
+++ exercises/062-getAllLetters/app.js
@@ -1,3 +1,10 @@
 function getAllLetters(str) {
     // your code here
+    let strArr = []
+
+    for(let i = 0; i < str.length; i++){
+        strArr.push(str[i])
+    }
+
+    return strArr
 }
