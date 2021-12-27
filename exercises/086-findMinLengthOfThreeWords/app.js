897c1834754e9dc4e9b9412822968c9cc2e9c164
ErnestoXG <ggoku02@gmail.com>
2021-12-27 23:09:57 +0000

    86 updated

--- exercises/086-findMinLengthOfThreeWords/app.js
+++ exercises/086-findMinLengthOfThreeWords/app.js
@@ -1,12 +1,12 @@
 // Write your function here
 
 function findMinLengthOfThreeWords(word1, word2, word3){
-    if(word1.length < word2.length && word1.length < .lenth){
-        return word1
-    }else if(word2.length < word1.length && word2.length < word3.lenth){
-        return word2
+    if(word1.length < word2.length && word1.length < word3.length){
+        return word1.length
+    }else if(word2.length < word1.length && word2.length < word3.length){
+        return word2.length
     }else{
-        return word3
+        return word3.length
     }
 }
 
