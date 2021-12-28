f947f617de8f8c35fa087701656b3f697627aca1
ErnestoXG <ggoku02@gmail.com>
2021-12-28 00:30:56 +0000

    Up to 90

--- exercises/089-getElementsThatEqual10AtProperty/app.js
+++ exercises/089-getElementsThatEqual10AtProperty/app.js
@@ -1,1 +1,18 @@
 // Write your function here
+function getElementsThatEqual10AtProperty(object, key){
+    let objArr = object[key]
+    let arr = []
+    
+    objArr.forEach(e => {
+        e == 10 ? arr.push(e) : null        
+    })
+
+    return arr
+}
+
+var obj = {
+    key: [1000, 10, 50, 10]
+};
+
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output); // --> [10, 10]
--- exercises/090-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/090-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,1 +1,17 @@
 // Write your function here
+function getElementsLessThan100AtProperty(obj, key){
+    let objArr = obj[key]
+    let finalArr = []
+
+    objArr.forEach(e=>{
+        e < 100 ? finalArr.push(e) : null
+    })
+
+    return finalArr
+}
+
+var obj = {
+    key: [1000, 20, 50, 500]
+};
+var output = getElementsLessThan100AtProperty(obj, 'key');
+console.log(output); // --> [20, 50]
